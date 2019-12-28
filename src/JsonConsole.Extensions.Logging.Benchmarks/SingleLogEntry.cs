31085d8da6847d0a265db892111a4976c9db9073
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2019-12-28 17:50:05 +0100

    Added Benchmark project

---
+++ src/JsonConsole.Extensions.Logging.Benchmarks/SingleLogEntry.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+using BenchmarkDotNet.Attributes;
+using Microsoft.Extensions.Logging;
+
+namespace JsonConsole.Extensions.Logging.Benchmarks
+{
+    [ShortRunJob, MemoryDiagnoser]
+    public class SingleLogEntry
+    {
+        private readonly ILogger _logger;
+
+        public SingleLogEntry()
+        {
+            var dateTime = new DateTime(2019, 12, 11, 20, 25, 0, DateTimeKind.Utc);
+            var loggerProvider = new JsonConsoleLoggerProvider(() => dateTime, Stream.Null);
+            _logger = loggerProvider.CreateLogger("MyCategory");
+        }
+
+        [Benchmark]
+        public void Run()
+        {
+            _logger.Log(LogLevel.Information, "MyMsg");
+        }
+    }
+}
